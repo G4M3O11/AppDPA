91cfb5483da86bd03606ddb4cdd79c063aa84881
David Alamo <24200433@ue.edu.pe>
2026-09-19 19:38:42 -0500

    Implement Navigation Drawer for the Home Screen

--- app/src/main/java/dev/davidalamo/appdpa/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/dev/davidalamo/appdpa/presentation/navigation/AppNavGraph.kt
@@ -18,6 +18,10 @@ fun AppNavGraph(){
     {
         composable("register") { RegisterScreen(navController) }
         composable("login") { LoginScreen(navController) }
-        composable("home") { HomeScreen() }
+        composable("home") {
+            DrawerScaffold(navController) {
+                HomeScreen()
+            }
+        }
     }
 }
